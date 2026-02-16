a044b439505998a1230241f659eb80f22026f462
Chirag-Ram <chiragram017@gmail.com>
2026-02-16 09:07:06 +0530

    Initial commit - UC1 Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp
+ * UC1 - Print "OOPS" to console
+ * @author kalees-bytes
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
